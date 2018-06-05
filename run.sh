cb7ec32096d2a09fba619ec085302d90947b56db
Ian Miell <ian.miell@gmail.com>
2018-06-05 16:24:53 +0100

    w

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ fi
 rm -f /tmp/a
 #$SHUTIT build --echo -d bash -m shutit-library/vagrant -m shutit-library/virtualization -l debug "$@"
 #../shutit/shutit build --pane -d bash -m shutit-library/vagrant -m shutit-library/virtualization "$@"
-../shutit/shutit build -l debug -d bash -m shutit-library/vagrant -m shutit-library/virtualization "$@"
+../shutit/shutit build -w -l warning -d bash -m shutit-library/vagrant -m shutit-library/virtualization "$@"
 if [[ $? != 0 ]]
 then
 	exit 1
